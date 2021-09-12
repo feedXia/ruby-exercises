3a7655dc65de92831d4a3b0e6a9899376ffe7b38
Xia <zeyaoliu@me.com>
2021-09-12 15:32:28 +0100

    S names answers

---
+++ s-names.rb
@@ -0,0 +1,9 @@
+# The user sees a greeting, which asks them to enter their name.
+# The user enters their name.
+# If the user's name begins with 'S', the program shouts the user's name back at them.
+# If the user's name begins with any other letter, the program just says 'Hi, ' plus their name.
+
+puts "What's your name?"
+name = gets.chomp.capitalize
+
+name.start_with?('S') ? (puts "#{name.upcase}!") : (puts "Hi, #{name}.")
