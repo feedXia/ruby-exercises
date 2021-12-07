f6e8af3fe5fa9302743672683d08dc49622255cd
Xia <zeyaoliu@me.com>
2021-12-07 12:57:40 +0000

    add katas

--- Codewars-Katas/duplicate-encoder.rb
+++ Codewars-Katas/duplicate-encoder.rb
@@ -11,7 +11,7 @@
 # Assertion messages may be unclear about what they display in some languages. If you read "...It Should encode XXX", the "XXX" is the expected result, not the input!
 
 def duplicate_encode(word)
-  p (1..word.length).split("")
+  p (1..word.length).map { |i| }
 end
 
 # Tests
